4f16bd566ce7159817f2044f36e0ef05b84290e6
Kane Castillano <castillano.kaneerryl@auf.edu.ph>
2022-10-14 15:54:14 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+// function alertMessage(match1, match2){
+//     alert(match1 + ' vs ' + match2);
+
+// }
+
+// function compute(value1, value2, value3){
+//     alert(value1 + value2 - value3);
+
+// }
+
+// function msg1(){
+//     let n = 'Hello';
+//     alert (n);
+// }
+
+// let h = "hello";
+
+// function msg2(){
+// alert(h + " world");
+// }
+
+// function compare(){
+//     let comp = 5 < 1;
+// alert(comp);
+// }
+
+// function age_checked(){
+//     let age =prompt("How old are you?", 0); 
+//     alert(`You are ${age} years old`);
+// }
+
+// function test_confirm(){
+//     let val = confirm("Are you sure you want to proceed?");
+//     alert(val);
+// }
+
+function condition(){
+    let valueA = prompt('Please input first number: ', '')
+    let valueB = prompt('Please input second number: ', '');
+    
+    if (valueA > valueB) {
+    alert('Value A is higher');
+    }
+    else if (valueA < valueB){
+    alert('Value B is higher');
+    }
+    else {
+    alert('Equal');
+    }
+}
+
+//unction login(){
+//     function condition(){
+//         let pass = prompt('Please enter password: ', '');
+        
+//         if (pass == 1234) 
+//         alert('Login successful');    
+// }
+//}
